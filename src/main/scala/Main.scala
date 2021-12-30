1c95e435f78e50b944bfa79f34cb94d02d302248
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-12-30 08:19:49 -0500

    Add top-level method/function definitions.
    To create Machine for ad-hoc testing. Need to get good with unit testing ... smh

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import mealy.model.*
   val init = new State
   val anotherOne = new State
   val trann = new Transition('a', '1', init, anotherOne)
-  val secondTran = new Transition('b', '2', anotherOne, init)
+  val abcde = new Transition('b', '2', anotherOne, init)
   val thirf = new Transition('b', '4', anotherOne, init)
   val forth = new Transition('b', '5', anotherOne, init)
 
@@ -13,9 +13,20 @@ import mealy.model.*
     init,
     Set('a', 'b'),
     Set('0', '1'),
-    List(trann, secondTran, thirf, forth)
+    List(trann, abcde, thirf, forth)
   )
 
-  mach.setInputSequence("ababababa")
-  mach.nonDeterministicConsume // Seems to work, next up:
+  mach setInputSequence "abbabababa"
+  // mach.nonDeterministicConsume
+
+  // println
+  val mach2 = getNDMachine
+  mach2 setInputSequence "abab"
+  // mach2.nonDeterministicConsume
   println
+
+  val mach3 = getDMachine
+
+  mach3 setInputSequence "bbaafb"
+
+  mach3.nonDeterministicConsume
--- src/main/scala/mealy/model/MealyUtils.scala
+++ src/main/scala/mealy/model/MealyUtils.scala
@@ -1,14 +1,60 @@
 package mealy.model
 
 @throws(classOf[NoTransitionFound])
-def getApplicableTransitions(argState: State,triggerChar: Char): List[Transition] =
-    def filterPredicate(transition: Transition): Boolean =
-      transition.isValid && transition.isTriggeredBy(triggerChar)
-    val retVal = argState.getOutgoing.filter(filterPredicate)
-    if retVal.isEmpty 
-      then 
-          throw new NoTransitionFound(
-            s"No transition found from this state: $argState, trigger: $triggerChar."
-          )
-    else
-      retVal
+def getApplicableTransitions(
+    argState: State,
+    triggerChar: Char
+): List[Transition] =
+  def filterPredicate(transition: Transition): Boolean =
+    transition.isValid && transition.isTriggeredBy(triggerChar)
+  val retVal = argState.getOutgoing.filter(filterPredicate)
+  if retVal.isEmpty then
+    throw new NoTransitionFound(
+      s"No transition found from this state: $argState, trigger: $triggerChar."
+    )
+  else retVal
+
+def getNDMachine: Machine =
+  val s1 = new State("s1")
+  val s2 = new State("s2")
+  val s3 = new State("s3")
+  val t1 = new Transition('a', '1', s1, s3)
+  val t2 = new Transition('a', '1', s1, s2)
+  val t3 = new Transition('b', '1', s1, s3)
+  val t4 = new Transition('b', '2', s1, s2)
+  val t5 = new Transition('a', '1', s2, s3)
+  val t6 = new Transition('a', '2', s2, s1)
+  val t7 = new Transition('b', '2', s2, s2)
+  val t8 = new Transition('b', '2', s3, s2)
+  val t9 = new Transition('b', '1', s3, s1)
+  val t10 = new Transition('a', '1', s3, s2)
+
+  Machine(
+    List(s1, s2, s3),
+    s1,
+    Set('a', 'b'),
+    Set('1', '2'),
+    List(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10)
+  )
+
+def getDMachine: Machine =
+  val d1 = new State("d1")
+  val d2 = new State("d2")
+  val d3 = new State("d3")
+  val d4 = new State("d4")
+  val t1 = new Transition('a', '1', d1, d2)
+  val t2 = new Transition('b', '2', d1, d4)
+  val t3 = new Transition('b', '1', d2, d1)
+  val t4 = new Transition('a', '2', d2, d2)
+  val t5 = new Transition('a', '1', d3, d2)
+  val t6 = new Transition('b', '1', d3, d1)
+  val t7 = new Transition('a', '1', d4, d1)
+  val t8 = new Transition('b', '2', d4, d3)
+  
+  Machine(
+    List(d1, d2, d3, d4),
+    d1,
+    Set('a', 'b'),
+    Set('1', '2'),
+    List(t1, t2, t3, t4, t5, t6, t7, t8)
+  )
