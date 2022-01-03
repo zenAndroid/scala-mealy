387794713f66ba9a98cfb81c263a60998b96154d
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2022-01-02 23:03:50 -0500

    Remove ad-hoc machine.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,26 +3,10 @@ import scala.util.Failure
 import scala.util.Success
 
 @main def hello: Unit =
-  val init       = new State
-  val anotherOne = new State
-  val trann      = new Transition('a', '1', init, anotherOne)
-  val abcde      = new Transition('b', '2', anotherOne, init)
-  val thirf      = new Transition('b', '4', anotherOne, init)
-  val forth      = new Transition('b', '5', anotherOne, init)
 
-  val mach = new Machine(
-    List(init, anotherOne),
-    init,
-    Set('a', 'b'),
-    Set('0', '1'),
-    List(trann, abcde, thirf, forth)
-  )
-
-  mach.setInputSequence("abbabababa")
-  // mach.nonDeterministicConsume
-
-  // println
   val mach2 = getNDMachine
-  mach2 setInputSequence "ababk"
+  mach2 setInputSequence "abababababababababababababab"
   // println(mach2.toDot)
-  mach2.nonDeterministicConsume
+  val consuVal = mach2.nonDeterministicConsume
+  println(consuVal(0))
+  println(consuVal(1))
